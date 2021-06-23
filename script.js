3874091698b2248712d4e39b89a903013645ab37
Rachel Fearon <rachel.fearon@gmail.com>
2021-06-22 22:22:52 -0400

    Add numbers eventListener, number display logic

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+const addButton = document.querySelector('#add');
+
+const oneButton = document.querySelector('#one');
+const twoButton = document.querySelector('#two');
+
+const display = document.querySelector('#display');
+
+const numberButtons = document.querySelector('#numberbuttons');
+
+let currentNumber = '';
+let secondNumber = undefined;
+let runningTotal = undefined;
+let currentOperator = '';
+
+numberButtons.addEventListener("click", event => {
+    let btnid = event.target.textContent;
+    storeNumber(btnid);
+});
+
+function displayNumber(btnid) {
+    let number = Number(btnid);
+    if (isNaN(number)) {
+        return;
+    } else {
+        let newNumber = (`${number}`);
+        let newDisplay = newNumber + currentNumber;
+
+        display.textContent = (`${currentNumber.toString()}${number}`); 
+
+        currentNumber = display.textContent;
+    };
+};
+
+function storeNumber(btnid) {
+    number = Number(btnid);
+    displayNumber(btnid);
+};
+
+function storeOperator(btnid) {
+    currentOperator = btnid.textContent;
+};
+
 
 function add(num1, num2) {
     return num1 + num2;
