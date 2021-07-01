5a8274d3f3d173145b7bd8771c00865801d1fb47
Rachel Fearon <rachel.fearon@gmail.com>
2021-07-01 17:01:31 -0400

    Restructure button wrappers

--- script.js
+++ script.js
@@ -6,15 +6,57 @@ const twoButton = document.querySelector('#two');
 const display = document.querySelector('#display');
 
 const numberButtons = document.querySelector('#numberbuttons');
+const operatorButtons = document.querySelector('#operatorbuttons');
 
-let currentNumber = '';
-let secondNumber = undefined;
-let runningTotal = undefined;
+let currentNumber = '0';
+let runningTotal = '';
 let currentOperator = '';
+let currentButton = '';
+
+let operatorButton = document.querySelector('.operatorbutton');
 
 numberButtons.addEventListener("click", event => {
     let btnid = event.target.textContent;
+
+    if (event.target.id === 'equals') {
+        if (currentOperator.textContent === '+') {
+            currentOperator.classList.toggle("activeButton");
+            return Number(currentNumber) + Number(display.textContent);
+        };
+    }; 
+
     storeNumber(btnid);
+});
+
+operatorButtons.addEventListener("click", event => {
+    // if (display.textContent != '0') {
+
+    // }
+
+    if (event.target.id === 'equals') {
+        if (currentOperator.textContent === '+') {
+            currentOperator.classList.toggle("activeButton");
+            return Number(currentNumber) + Number(display.textContent);
+        };
+    } else if (currentOperator === '') {
+        let operatorId = document.querySelector(`#${event.target.id}`);
+        storeOperator(operatorId);
+        currentButton = document.querySelector(`#${event.target.id}`);
+    } else  if (currentOperator === document.querySelector(`#${event.target.id}`)) {
+        currentOperator = '';
+        currentButton = '';
+    } else {
+        let operatorId = document.querySelector(`#${event.target.id}`);;
+        currentOperator.classList.toggle("activeButton");
+        storeOperator(operatorId);
+        currentButton = document.querySelector(`#${event.target.id}`);
+    };
+    
+    if (event.target.id === 'operatorbuttons') {
+        return;
+    } else {
+    event.target.classList.toggle("activeButton");
+    };
 });
 
 function displayNumber(btnid) {
@@ -22,12 +64,19 @@ function displayNumber(btnid) {
     if (isNaN(number)) {
         return;
     } else {
-        let newNumber = (`${number}`);
-        let newDisplay = newNumber + currentNumber;
+        if (currentNumber.toString() === '0') {
+            display.textContent = (`${number}`); 
+            currentNumber = display.textContent;
+        } else if (currentOperator != '') {
+            display.textContent = (`${number}`);
+            currentNumber = display.textContent;
+            //currentOperator.classList.toggle("activeButton");
+            operate(currentOperator, currentNumber, `${Number(display.textContent)}`);
 
-        display.textContent = (`${currentNumber.toString()}${number}`); 
-
-        currentNumber = display.textContent;
+        } else {
+            display.textContent = (`${currentNumber.toString()}${number}`); 
+            currentNumber = display.textContent;
+        }
     };
 };
 
@@ -36,8 +85,9 @@ function storeNumber(btnid) {
     displayNumber(btnid);
 };
 
-function storeOperator(btnid) {
-    currentOperator = btnid.textContent;
+function storeOperator(operatorId) {
+    currentOperator = operatorId;
+    
 };
 
 
@@ -58,7 +108,8 @@ function divide(num1, num2) {
 };
 
 function operate(operator, num1, num2) {
-    if (operator === '+') {
+    if (operator.id === 'add') {
+        currentOperator = '';
         return add(num1, num2);
     } else if (operator === '-') {
         return subtract(num1, num2);
